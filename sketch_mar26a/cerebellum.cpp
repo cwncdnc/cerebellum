d8af04300cc9e0172fac9ea75b98c6a1cf087bff
Charles William Carsten <74852165+cwncdnc@users.noreply.github.com>
2021-03-26 20:00:48 -0700

    connected vcode

--- sketch_mar26a/cerebellum.cpp
+++ sketch_mar26a/cerebellum.cpp
@@ -1,8 +1,18 @@
+#include <pRNG.h>
+
 #include <StaticThreadController.h>
 #include <Thread.h>
 #include <ThreadController.h>
+
 #include "cerebellum.h"
 
+pRNG prng;
+
 void cerebellum(){
-  
+  int vcode = prng.getRndInt();
+  switch(vcode){
+    
+    default:break;
+  }
+  //Serial.write(27);
 }
