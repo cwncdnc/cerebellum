9b7a632b55daa87b54c01052ffa52c18e7b37dc9
Charles William Carsten <74852165+cwncdnc@users.noreply.github.com>
2021-03-26 18:49:22 -0700

    Create sketch_mar26a.ino

---
+++ sketch_mar26a/sketch_mar26a.ino
@@ -0,0 +1,15 @@
+/* BN: Unknown board
+VID: 1A86
+PID: 7523
+SN: Upload any sketch to obtain it
+ */
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
